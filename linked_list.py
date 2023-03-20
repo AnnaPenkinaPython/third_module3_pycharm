5b7c728644e8c2c3a92df15c80fe9b8f064c4ebf
AnnaPenkinaPython <annapenkina200121@mail.ru>
2023-03-20 16:21:34 +0700

    5 task

--- linked_list.py
+++ linked_list.py
@@ -2,10 +2,6 @@ class Node:
     def __init__(self, data=None, next_node=None):
         self.data = data
         self.next_node = next_node
-        data = []
-
-    def __repr__(self):
-        return self.data
 
 
 class LinkedList:
@@ -13,29 +9,23 @@ class LinkedList:
         self.head = head
         self.tail = tail
 
-    def insert_beginning(self):
-        if self.head is None:
-            print("linked list is empty")
-        else:
-            n = self.head
-            while n is not None:
-                print(n.data)
-                n = n.next_node
-
-    def add_begin(self, data):
-        new_node = Node(data)
-        new_node.next_node = self.head
-        self.head = new_node
-
-    def insert_at_end(self, data):
-        new_node = Node(data)
+    def insert_beginning(self, x):
+        new_node = Node(x)
         if self.head is None:
             self.head = new_node
+            self.tail = new_node
         else:
-            n = self.head
-            while n.next_node is not None:
-                n = n.next_node
-            n.next_node = next_node
+            new_node.next_node = self.head
+            self.head = new_node
+
+    def insert_at_end(self, x):
+        new_node = Node(x)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
 
     def print_ll(self):
         ll_string = ''
@@ -48,6 +38,3 @@ class LinkedList:
 
         ll_string += ' None'
         print(ll_string)
-
-
-
--- test_main.py
+++ test_main.py
@@ -2,6 +2,10 @@ import unittest
 from main import *
 from custom_queue import Queue
 from custom_queue import *
+from io import StringIO
+import LinkedList, Node, ll
+from contextlib import redirect_stdout
+
 """тесты"""
 n1 = Node(5, None)
 n2 = Node('a', n1)
@@ -14,22 +18,21 @@ stack.push('data3')
 stack.pop()
 
 
-
 class TestMain(unittest.TestCase):
 
-        def test_Node(self):
-            self.assertEqual(n1.data, 5)
-            self.assertEqual(n2.data, 'a')
-            self.assertEqual(n1.data, 5)
-            self.assertEqual(n1.next_node, None)
+    def test_Node(self):
+        self.assertEqual(n1.data, 5)
+        self.assertEqual(n2.data, 'a')
+        self.assertEqual(n1.data, 5)
+        self.assertEqual(n1.next_node, None)
 
-        def test_Stack(self):
-            self.assertEqual(stack.top.data, 'data2')
-            self.assertEqual(stack.top.next_node.data, 'data1')
-            self.assertEqual(stack.top.next_node.next_node, None)
+    def test_Stack(self):
+        self.assertEqual(stack.top.data, 'data2')
+        self.assertEqual(stack.top.next_node.data, 'data1')
+        self.assertEqual(stack.top.next_node.next_node, None)
 
-        def test_pop(self):
-            self.assertEqual(stack.top.data, 'data2')
+    def test_pop(self):
+        self.assertEqual(stack.top.data, 'data2')
 
 
 class TestQueue(unittest.TestCase):
@@ -48,3 +51,17 @@ class TestQueue(unittest.TestCase):
         self.assertEqual(queue.dequeue, 'data2')
         self.assertEqual(queue.dequeue, 'data3')
 
+
+class Linked_listtest(unittest.TestCase):
+
+    def test_print_ll(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 0})
+        expected_output = "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None"
+        f = StringIO()
+        with redirect_stdout(f):
+            ll.print_ll()
+        self.assertEqual(f.getvalue().strip(), expected_output)
